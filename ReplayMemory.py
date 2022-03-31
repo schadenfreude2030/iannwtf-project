b1d3fb159c45fa36cfcfe94eeeccd27a03deedc1
TimNiklasWitte <wittet@uos.de>
2022-03-31 10:53:27 +0200

    More comments in ReplayMemory

--- ReplayMemory.py
+++ ReplayMemory.py
@@ -2,7 +2,13 @@ import numpy as np
 
 
 class ReplayMemory:
-    def __init__(self, capacity, input_dims):
+    def __init__(self, capacity: int, input_dims: tuple):
+        """Init the ReplayMemory.
+
+        Keyword arguments:
+        capacity -- maximal amount of buffer entrys
+        input_dims -- dimension of a game state (previous or current)
+        """
         self.capacity = capacity
         self.idx = 0
         self.idx_was_overflown = False
@@ -15,7 +21,17 @@ class ReplayMemory:
 
         self.done_flags = np.zeros(self.capacity, dtype=np.int32)
 
-    def store_experience(self, state, action, next_state, reward, done_flag):
+    def store_experience(self, state: np.array, action: int, next_state: np.array, reward: float, done_flag: bool):
+        """Store a experience in the ReplayMemory.
+        A experience consists of a state, an action, a next_state and a reward.
+
+        Keyword arguments:
+        state -- game state 
+        action -- action taken in state
+        next_state -- the new/next game state: in state do action -> next_state
+        reward -- reward received
+        done_flag -- does the taken action end the game?
+        """
 
         self.states[self.idx] = state
         self.actions[self.idx] = action
@@ -25,18 +41,28 @@ class ReplayMemory:
         self.done_flags[self.idx] = 1 - int(done_flag)
 
         self.idx += 1
+        # overflow handling -> reset idx to store entries
         if self.capacity <= self.idx:
             self.idx_was_overflown = True
             self.idx = 0
 
-    def sample_batch(self, batch_size):
+    def sample_batch(self, batch_size: int):
+        """Samples a random batch of entry of the ReplayMemory.
+
+        Keyword arguments:
+        batch_size -- size of the batch which is sampled
+
+        Return:
+        state, action, next_state, reward 
+        each of them as an np.array
+        """
 
         if self.idx_was_overflown:
             max_mem = self.capacity
         else:
             max_mem = self.idx
 
-        # Thompson sampling
+        # Sampling process
         rewards = self.rewards[:max_mem]
         # Normalize between [0,1] 
         rewards_z = (rewards - np.min(rewards)) / (np.max(rewards) - np.min(rewards))
@@ -55,4 +81,10 @@ class ReplayMemory:
         return states, actions, next_state, rewards, done_flag
 
     def have_enough_samples(self):
+        """Checks if 250000 or more entrys are in the ReplayMemory
+
+        Return:
+        true if 250000 or more entrys are in the ReplayMemory
+        """
+
         return self.idx_was_overflown or 250000 < self.idx
