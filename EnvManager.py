a15e31ddd90e35e12434ed32b225dd9e535ba998
TimNiklasWitte <wittet@uos.de>
2022-03-31 11:48:01 +0200

    Add more comments to EnvManager

--- EnvManager.py
+++ EnvManager.py
@@ -6,19 +6,36 @@ import numpy as np
 
 class EnvMananger:
 
-    def __init__(self, window_mode):
+    def __init__(self, window_mode: int):
+        """Init the EnvMananger. The input of the ANN is a concatenated version of the
+        current and next game state which is managed by this helper class.
+        It is a wrapper around the FlappyBirdGym.
+
+        Keyword arguments:
+        window_mode -- 0 = no window, 1 = only game window, 2 = game window with plots
+        """
+
         self.gym = FlappyBirdGym(window_mode)
 
         # Provide easy access to window
         if window_mode != WindowMode.NO_WINDOW:
             self.window = self.gym.window
 
-            # env state = previous and last state of original env (gym) -> detect moving direction etc.
+        # env state = previous and last state of original env (gym) -> detect moving direction etc.
         current_state = self.gym.get_state()
         self.state = np.zeros(shape=(*current_state.shape, 2))
         self.state[:, 0] = current_state
 
-    def step(self, action):
+    def step(self, action: int):
+        """Interacts with the encapsulated environment. 
+        By relaying the action the this environment.
+
+        Keyword arguments:
+        action -- action of the bird: 0 = no jump, 1 = jump
+
+        Return:
+        concatenated current and next game state, reward and "does this action end the game?"
+        """
         current_state, reward, done_flag = self.gym.step(action)
         self.state[:, 0] = self.state[:, 1]
         self.state[:, 1] = current_state
@@ -26,6 +43,9 @@ class EnvMananger:
         return self.state.flatten(), reward, done_flag
 
     def reset(self):
+        """Call reset on the environment.
+        Note that, current and next game state are also resetted.
+        """
         self.gym.reset()
 
         # reset env state
@@ -36,15 +56,30 @@ class EnvMananger:
         return self.state.flatten()
 
     def get_state(self):
+        """
+        Returns the concatenated version of the current and next game state (flatten).
+
+        Return:
+        current and game state as flatten. Shape: (24,)
+        """
         return self.state.flatten()
 
     def close(self):
+        """Call close on the environment.
+        """
         self.gym.close()
 
     @property
     def observation_space_shape(self):
+        """
+        Return: Return the shape of the current and next game state (flatten)
+        which is (24,).
+        """
         return self.state.flatten().shape
 
     @property
     def num_actions(self):
+        """
+        Return: Return the number of action of the gym which are 2 (no jump and jump)
+        """
         return self.gym.num_actions
